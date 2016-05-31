6e65430933bc44b78c59cfbf5660875c18a25cc8
keeho.yang <keeho.yang@samsung.com>
2016-05-31 18:04:40 +0900

    add aslr_test, dep_test, setuid_test, root_test, smack_rule_test, path_check_test
    Change-Id: I4d54527e7061b42222e3f5a36d04fd296bd28d34

---
+++ test/smack_rule_test/checksmacklabel.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+result_dir="/usr/share/security-config/result"
+log_dir="/usr/share/security-config/log"
+result_file=$result_dir"/checksmacklabel_result.csv"
+log_file=$log_dir"/checksmacklabel_log.csv"
+
+function CHECK_RULE_ACCESS
+{
+	if [ "${label:8:1}"  != '_' ] && [ "${label:8:1}"  != '*' ] && [ "${label:8:1}"  != '^' ] &&
+		 [ "${label:8:6}"  != 'System' ] && [ "${label:8:11}"  != 'System::Run' ] && [ "${label:8:11}"  != 'System::Log' ] &&
+	   [ "${label:8:13}"  != 'System::Shared' ] && [ "${label:8:4}"  != 'User' ] && [ "${label:8:10}"  != 'User::Home' ] &&
+	   [ "${label:8:17}"  != 'User::App::Shared' ] && [ "${label:8:9}"  != 'User::Pkg' ]
+	then
+		/bin/echo "ACCESS label,$line2" >> $log_file
+  fi
+}
+
+function CHECK_RULE_EXECUTE
+{
+	if [ "${label:9:1}"  != '_' ] && [ "${label:9:1}"  != '^' ] &&
+	   [ "${label:9:6}"  != 'System' ] && [ "${label:9:4}"  != 'User' ] && [ "${label:9:9}"  != 'User::App' ]
+	then
+		/bin/echo "EXECUTE label,$line2" >> $log_file
+	fi
+}
+
+function LABEL_CHECK
+{    
+	/usr/bin/chsmack $1/* | while read line2 
+	do
+		label=$(/bin/echo $line2 | /usr/bin/rev | /usr/bin/cut -f1 -d " " | /usr/bin/rev)
+		if [ "${label:0:6}" == 'access' ]
+		then
+			CHECK_RULE_ACCESS
+		elif [ "${label:0:7}" == 'execute' ] 
+		then
+			CHECK_RULE_EXECUTE
+			label=$(/bin/echo $line2 | /usr/bin/rev | /usr/bin/cut -f2 -d " " | /usr/bin/rev)
+			CHECK_RULE_ACCESS
+		elif [ "${label:0:9}" == 'transmute' ] 
+		then
+			label=$(/bin/echo $line2 | /usr/bin/rev | /usr/bin/cut -f2 -d " " | /usr/bin/rev)
+			if [ "${label:0:6}" == 'access' ]
+			then
+				CHECK_RULE_ACCESS
+			elif [ "${label:0:7}" == 'execute' ] 
+			then
+				CHECK_RULE_EXECUTE
+				label=$(/bin/echo $line2 | /usr/bin/rev | /usr/bin/cut -f3 -d " " | /usr/bin/rev)
+				CHECK_RULE_ACCESS	
+			fi	     
+		fi
+	done
+}
+
+function SMACK_LABEL_CHECK
+{
+	/usr/bin/find / -type d 2>/dev/null | while read line  # Remove error print
+	do
+		LABEL_CHECK $line 
+	done
+}
+
+if [ ! -d $log_dir ]; then
+    /bin/mkdir $log_dir
+fi
+if [ ! -d $result_dir ]; then
+    /bin/mkdir $result_dir
+fi
+
+if [ -e $result_file ]
+then
+	/bin/rm $result_file
+fi
+if [ -e $log_file ]
+then
+	/bin/rm $log_file
+fi
+
+/bin/echo "SMACK LABEL CHECK STARTED!"
+
+SMACK_LABEL_CHECK 
+
+if [ ! -e $log_file ]
+then
+	/bin/echo "TC PASS?, YES, There is no wrong smack label." >> $result_file
+else
+	/bin/echo "TC PASS?, NO, There are wrong smack labels." >> $result_file
+fi
+
+/bin/echo "Generated checksmacklabel.csv / SMACK LABEL CHECK FINISHED!"
