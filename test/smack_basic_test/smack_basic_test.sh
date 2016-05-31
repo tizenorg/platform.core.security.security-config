d78eeb8c8e0037ca46a3d716bfb4c6abde13e659
keeho.yang <keeho.yang@samsung.com>
2016-05-31 18:04:40 +0900

    add aslr_test, dep_test, setuid_test, root_test, smack_rule_test, path_check_test
    Change-Id: I4d54527e7061b42222e3f5a36d04fd296bd28d34

---
+++ test/smack_basic_test/smack_basic_test.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+result_dir="/usr/share/security-config/result"
+log_dir="/usr/share/security-config/log"
+result_file=$result_dir"/smack_basic_result.csv"
+log_file=$log_dir"/smack_basic_log.csv"
+smackfs_path="/sys/fs/smackfs"
+smack_rule_path="$smackfs_path/load2"
+
+# check whether smackfs is mounted
+function chk_smackfs_mount
+{
+	/bin/echo "1. smackfs mount check" >> $log_file
+	result_smackfs_mount=$(/bin/cat /proc/mounts | /bin/grep "$smackfs_path")
+	if [ "$result_smackfs_mount" = "" ]
+	then
+		/bin/echo "Is smackfs mounted? , No" >> $log_file
+		/bin/echo "0" > $result_file
+	else
+		/bin/echo "Is smackfs mounted? , Yes" >> $log_file
+	fi
+}
+
+function chk_floor_smack_rule
+{
+	flag=0
+	/bin/echo "" >> $log_file
+	/bin/echo "2. Smack rule for floor label check" >> $log_file	
+
+	while read app_name
+	do
+		app_name_cut=$(/bin/echo ${app_name:4}) # cut "app_"
+		rule_filtered=$(/bin/cat $smack_rule_path | grep "User::App::$app_name_cut _ l")
+		if [ "$rule_filtered" = "" ]
+		then
+			flag=1
+			/bin/echo "User::App::$app_name_cut _ l is not existed" >> $log_file
+		fi
+	done < <( /bin/ls /opt/var/security-manager/rules | grep "app_")
+
+	if [ $flag -eq 0 ]
+	then
+		/bin/echo "Floor check pass? , Yes" >> $log_file
+	else
+		/bin/echo "Floor check pass? , No" >> $log_file
+		/bin/echo "0" > $result_file
+	fi
+}
+
+# check whether long label is supported
+function chk_long_label
+{
+	/bin/echo "" >> $log_file
+	/bin/echo "3. Long label support check" >> $log_file
+	if [ -e "$smackfs_path/load2" ]
+	then
+		/bin/echo "Is /sys/fs/smackfs/load2 existed?, Yes" >> $log_file
+	else
+		/bin/echo "Is /sys/fs/smackfs/load2 existed?, No" >> $log_file
+		/bin/echo "0" > $result_file
+	fi
+	
+	if [ -e "$smackfs_path/access2" ]
+	then
+		/bin/echo "Is /sys/fs/smackfs/access2 existed?, Yes" >> $log_file
+	else
+		/bin/echo "Is /sys/fs/smackfs/access2 existed?, No" >> $log_file
+		/bin/echo "0" > $result_file
+	fi
+}
+
+function chk_netlabel
+{
+	/bin/echo "" >> $log_file
+	/bin/echo "4. Netlabel support check" >> $log_file
+	if [ -e "$smackfs_path/netlabel" ]
+	then
+		/bin/echo "Is /sys/fs/smackfs/netlabel existed?, Yes" >> $log_file
+	else
+		/bin/echo "Is /sys/fs/smackfs/netlabel existed?, No" >> $log_file
+		/bin/echo "0" > $result_file
+	fi
+	
+	if [ -e "$smackfs_path/ambient" ]
+	then
+		/bin/echo "Is /sys/fs/smackfs/ambient existed?, Yes" >> $log_file
+	else
+		/bin/echo "Is /sys/fs/smackfs/ambient existed?, No" >> $log_file
+		/bin/echo "0" > $result_file
+	fi
+}
+
+function chk_ptrace
+{
+	/bin/echo "" >> $log_file
+	/bin/echo "5. Ptrace smack option check" >> $log_file
+	ptrace_read=$(/bin/cat "$smackfs_path/ptrace")
+
+	if [ $ptrace_read -eq 0 ]
+	then
+		/bin/echo "Read sys/fs/smackfs/ptrace , 0 - default" >> $log_file
+		/bin/echo "0" > $result_file
+	elif [ $ptrace_read -eq 1 ]
+	then
+		/bin/echo "Read sys/fs/smackfs/ptrace , 1 - exact" >> $log_file
+	elif [ $ptrace_read -eq 2 ]
+	then
+		/bin/echo "Read sys/fs/smackfs/ptrace , 2 - draconian" >> $log_file
+	else
+		/bin/echo "Read sys/fs/smackfs/ptrace , $ptrace_read : invalid value" >> $log_file
+		/bin/echo "0" > $result_file		
+	fi
+}
+
+function chk_unconfined
+{
+	/bin/echo "" >> $log_file
+	/bin/echo "6. unconfined check" >> $log_file
+	if [ -e "$smackfs_path/unconfined" ]
+	then
+		unconfined_read=$(/bin/cat $smackfs_path/unconfined)
+		if [ "$unconfined_read" = "" ]
+		then
+			/bin/echo "unconfied file is existed. But it is empty." >> $log_file
+		else
+			/bin/echo "unconfied file is existed. And it is not empty." >> $log_file
+			/bin/echo "0" > $result_file
+		fi
+	else
+		/bin/echo "unconfied file is not existed." >> $log_file
+	fi
+}
+
+if [ ! -d $log_dir ]; then
+    /bin/mkdir $log_dir
+fi
+if [ ! -d $result_dir ]; then
+    /bin/mkdir $result_dir
+fi
+
+if [ -e $result_file ]
+then
+	/bin/rm $result_file
+fi
+if [ -e $log_file ]
+then
+	/bin/rm $log_file
+fi
+
+/bin/echo "1" > $result_file
+chk_smackfs_mount
+chk_floor_smack_rule
+chk_long_label
+chk_netlabel
+chk_ptrace
+chk_unconfined
+
+
---
+++ test/smack_rule_test/checksmackrule_loaded.sh
@@ -0,0 +1,390 @@
+#!/bin/bash
+
+result_dir="/usr/share/security-config/result"
+log_dir="/usr/share/security-config/log"
+SMACK_RULE_APPLY_PATH="/sys/fs/smackfs/load2"
+dbpath="/opt/dbspace/.security-manager.db"
+result_file=$result_dir"/checksmackrule_loaded_result.csv"
+log_file=$log_dir"/checksmackrule_loaded_log.csv"
+
+function RULE_CHECK
+{
+    # System ~PKG~ rwxat
+    if [ "$1" == "System" ] && [[ "$2" == *"::Pkg::"* ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi    
+    # System ~PKG~::RO rwxat
+    elif [ "$1" == "System" ] && [[ "$2" == *"::Pkg::"*"::RO" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # System ~PKG~::SharedRO rwxat
+    elif [ "$1" == "System" ] && [[ "$2" == *"::Pkg::"*"::SharedRO" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # User ~PKG~ rwxat
+    elif [ "$1" == "User" ] && [[ "$2" == *"::Pkg::"* ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # User ~PKG~::RO rwxat
+    elif [ "$1" == "User" ] && [[ "$2" == *"::Pkg::"*"::RO" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # User ~PKG~::SharedRO rwxat
+    elif [ "$1" == "User" ] && [[ "$2" == *"::Pkg::"*"::SharedRO" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # System User::App::Shared rwxat
+    elif [ "$1" == "System" ] && [[ "$2" == "User::App::Shared" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # System ~APP~ rwx
+    elif [ "$1" == "System" ] && [[ "$2" == *"::App::"* ]]
+    then
+        if [ "$3" == "rwx" ]
+        then
+            return 0
+        fi            
+    # ~APP~ System wx
+    elif [[ "$1" == *"::App::"* ]] && [ "$2" == "System" ]
+    then
+        if [ "$3" == "wx" ]
+        then
+            return 0
+        fi          
+    # ~APP~ System::Shared rxl
+    elif [[ "$1" == *"::App::"* ]] && [ "$2" == "System::Shared" ]
+    then
+        if [ "$3" == "rxl" ]
+        then
+            return 0
+        fi   
+    # ~APP~ System::Run rwxat
+    elif [[ "$1" == *"::App::"* ]] && [ "$2" == "System::Run" ]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi   
+    # ~APP~ System::Log rwxa
+    elif [[ "$1" == *"::App::"* ]] && [ "$2" == "System::Log" ]
+    then
+        if [ "$3" == "rwxa" ]
+        then
+            return 0
+        fi  
+    # ~APP~ _ l
+    elif [[ "$1" == *"::App::"* ]] && [ "$2" == "_" ]
+    then
+        if [ "$3" == "l" ]
+        then
+            return 0
+        fi  
+    # ~APP~ User::App::Shared rwxat
+    elif [[ "$1" == *"::App::"* ]] && [ "$2" == "User::App::Shared" ]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi
+    # User User::App::Shared rwxat
+    elif [ "$1" == "User" ] && [[ "$2" == "User::App::Shared" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # User ~APP~ rwx
+    elif [ "$1" == "User" ] && [[ "$2" == *"App"* ]]
+    then
+        if [ "$3" == "rwx" ]
+        then
+            return 0
+        fi  
+    # ~APP~ User wx
+    elif [[ "$1" == *"::App::"* ]] && [ "$2" == "User" ]
+    then
+        if [ "$3" == "wx" ]
+        then
+            return 0
+        fi     
+    # ~APP~ User::Home rxl
+    elif [[ "$1" == *"::App::"* ]] && [ "$2" == "User::Home" ]
+    then
+        if [ "$3" == "rxl" ]
+        then
+            return 0
+        fi  
+    # SharedRO
+    # ~App~ ~Pkg~::SharedRO rwxat : same app and pkg
+    # ~App~ ~Pkg~::SharedRO rx : diffrent app and pkg
+    elif [[ "$1" == *"::App::"* ]] && [[ "$2" == *"::Pkg::"*"::SharedRO" ]]
+    then
+        pkgname=$(/bin/echo $2 | /usr/bin/cut -f 5 -d ":")
+        appname=$(/bin/echo $1 | /usr/bin/cut -f 5 -d ":")
+        pkgname_db=$(/usr/bin/sqlite3 $dbpath "select DISTINCT pkg_name from app_pkg_view where app_name='$appname';")
+        
+        if [ "$pkgname" == "$pkgname_db" ]
+        then
+            if [ "$3" == "rwxat" ]
+            then
+                return 0
+            fi
+        else
+            if [ "$3" == "rx" ]
+            then
+                return 0
+            fi
+        fi 
+    # ~APP~ ~PKG~::RO rxl
+    elif [[ "$1" == *"::App::"* ]] && [[ "$2" == *"::Pkg::"*"::RO" ]]
+    then
+        pkgname=$(/bin/echo $2 | /usr/bin/cut -f 5 -d ":")
+        appname=$(/bin/echo $1 | /usr/bin/cut -f 5 -d ":")
+        pkgname_db=$(/usr/bin/sqlite3 $dbpath "select DISTINCT pkg_name from app_pkg_view where app_name='$appname';")
+        
+        if [ "$pkgname" == "$pkgname_db" ]
+        then
+            if [ "$3" == "rxl" ]
+            then
+                return 0
+            fi
+        fi  
+    # ~APP~ ~PKG~ rwxat
+    elif [[ "$1" == *"::App::"* ]] && [[ "$2" == *"::Pkg::"* ]]
+    then
+        pkgname=$(/bin/echo $2 | /usr/bin/cut -f 5 -d ":")
+        appname=$(/bin/echo $1 | /usr/bin/cut -f 5 -d ":")
+        pkgname_db=$(/usr/bin/sqlite3 $dbpath "select DISTINCT pkg_name from app_pkg_view where app_name='$appname';")
+        
+        if [ "$pkgname" == "$pkgname_db" ]
+        then
+            if [ "$3" == "rwxat" ]
+            then
+                return 0
+            fi
+        fi  
+    # ~APP~ ~AUTHOR~ rwxat
+    elif [[ "$1" == *"::App::"* ]] && [[ "$2" == *"Author"* ]]
+    then
+        authorID=$(/bin/echo $2 | /usr/bin/cut -f 5 -d ":")
+        appname=$(/bin/echo $1 | /usr/bin/cut -f 5 -d ":")
+        authorID_db=$(/usr/bin/sqlite3 $dbpath "select DISTINCT author_id from app_pkg_view where app_name='$appname';")
+        
+        if [ "$authorID" == "$authorID_db" ]
+        then
+            if [ "$3" == "rwxat" ]
+            then
+                return 0
+            fi
+        fi  
+    # User ~AUTHOR~ rwxat
+    elif [ "$1" == "User" ] && [[ "$2" == *"Author"* ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi  
+    # System ~AUTHOR~ rwxat
+    elif [ "$1" == "System" ] && [[ "$2" == *"Author"* ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi  
+    # From here, default 3-Domain Rule Check
+    # _ System rwxa
+    elif [ "$1" == "^" ] && [[ "$2" == "System" ]]
+    then
+        if [ "$3" == "rwxa" ]
+        then
+            return 0
+        fi  
+    # ^ System::Log rwxa
+    elif [ "$1" == "^" ] && [[ "$2" == "System::Log" ]]
+    then
+        if [ "$3" == "rwxa" ]
+        then
+            return 0
+        fi  
+    # ^ System::Run rwxat
+    elif [ "$1" == "^" ] && [[ "$2" == "System::Run" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi  
+    # _ System wx
+    elif [ "$1" == "_" ] && [[ "$2" == "System" ]]
+    then
+        if [ "$3" == "wx" ]
+        then
+            return 0
+        fi 
+    # _ System::Run rwxat
+    elif [ "$1" == "_" ] && [[ "$2" == "System::Run" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # System System::Log rwxa
+    elif [ "$1" == "System" ] && [[ "$2" == "System::Log" ]]
+    then
+        if [ "$3" == "rwxa" ]
+        then
+            return 0
+        fi 
+    # System System::Run rwxat
+    elif [ "$1" == "System" ] && [[ "$2" == "System::Run" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # System System::Shared rwxat
+    elif [ "$1" == "System" ] && [[ "$2" == "System::Shared" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # System User rwx
+    elif [ "$1" == "System" ] && [[ "$2" == "User" ]]
+    then
+        if [ "$3" == "rwx" ]
+        then
+            return 0
+        fi 
+    # System User::Home rwxat
+    elif [ "$1" == "System" ] && [[ "$2" == "User::Home" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # System _ rxl
+    elif [ "$1" == "System" ] && [[ "$2" == "_" ]]
+    then
+        if [ "$3" == "rxl" ]
+        then
+            return 0
+        fi 
+    # User _ rxl
+    elif [ "$1" == "User" ] && [[ "$2" == "_" ]]
+    then
+        if [ "$3" == "rwl" ]
+        then
+            return 0
+        fi 
+    # User System wx
+    elif [ "$1" == "User" ] && [[ "$2" == "System" ]]
+    then
+        if [ "$3" == "wx" ]
+        then
+            return 0
+        fi 
+    # User System::Run rwxat
+    elif [ "$1" == "User" ] && [[ "$2" == "System::Run" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # User System::Log rwxa
+    elif [ "$1" == "User" ] && [[ "$2" == "System::Log" ]]
+    then
+        if [ "$3" == "rwxa" ]
+        then
+            return 0
+        fi 
+    # User System::Shared rxl
+    elif [ "$1" == "User" ] && [[ "$2" == "System::Shared" ]]
+    then
+        if [ "$3" == "rxl" ]
+        then
+            return 0
+        fi 
+    # User User::Home rwxat
+    elif [ "$1" == "User" ] && [[ "$2" == "User::Home" ]]
+    then
+        if [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    # app <-> app rwxat cross package
+    elif [[ "$1" == *"::App::"* ]] && [[ "$2" == *"::App::"* ]]
+    then
+        appname1=$(/bin/echo $1 | /usr/bin/cut -f 5 -d ":")
+        appname2=$(/bin/echo $2 | /usr/bin/cut -f 5 -d ":")
+        pkgname1=$(/usr/bin/sqlite3 $dbpath "select DISTINCT pkg_name from app_pkg_view where app_name='$appname1';")
+        pkgname2=$(/usr/bin/sqlite3 $dbpath "select DISTINCT pkg_name from app_pkg_view where app_name='$appname2';")
+        if [ "$pkgname1" == "$pkgname2" ] && [ "$3" == "rwxat" ]
+        then
+            return 0
+        fi 
+    fi
+
+    echo "$1,$2,$3" >> $log_file
+}
+
+function RULE_CHECK_APPLY_PATH
+{
+    /usr/bin/cat $SMACK_RULE_APPLY_PATH | while read line    
+    do
+        subject=$(/bin/echo $line | /usr/bin/cut -f 1 -d " ")
+        object=$(/bin/echo $line | /usr/bin/cut -f 2 -d " ")
+        rule=$(/bin/echo $line | /usr/bin/cut -f 3 -d " ")
+
+        RULE_CHECK $subject $object $rule    
+    done
+}
+
+if [ ! -d $log_dir ]; then
+    /bin/mkdir $log_dir
+fi
+if [ ! -d $result_dir ]; then
+    /bin/mkdir $result_dir
+fi
+
+if [ -e $result_file ]
+then
+	/bin/rm $result_file
+fi
+if [ -e $log_file ]
+then
+	/bin/rm $log_file
+fi
+
+/bin/echo "SMACK RULE CHECK STARTED!"
+
+RULE_CHECK_APPLY_PATH
+
+if [ ! -e $log_file ]
+then
+	/bin/echo "1" >> $result_file
+else
+	/bin/echo "0" >> $result_file
+fi
+
+/bin/echo "Generated checksmackrule_loaded.csv / SMACK RULE CHECK FINISHED! "
